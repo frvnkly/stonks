1d4c6e5199218e84251b2d16a9c5eb5aaa33d8e7
Frank Li <li.26.frank@gmail.com>
2019-08-07 02:09:42 -0400

    sign up form and change handlers and react hooks work

---
+++ client/components/SignupForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const signupFormHandler = e => {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={signupFormHandler}>
+      <p>Email</p>
+      <input 
+        type='text' 
+        value={email} 
+        onChange={e => setEmail(e.target.value)} 
+      />
+      <p>Password</p>
+      <input
+        type='password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <br />
+      <input type='submit' value='Submit' />
+    </form>
+  );
+};
