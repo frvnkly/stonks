0475bb0b945fc7655478a30c69cb3e95705e62db
Frank Li <li.26.frank@gmail.com>
2019-08-27 03:49:12 -0400

    TransactionList displays transaction history on transactions page

---
+++ client/components/TransactionList.js
@@ -0,0 +1,81 @@
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  card: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  timestamp: {
+    color: 'gray',
+    flexBasis: '100%',
+  },
+  column: {
+    flexBasis: '25%',
+  },
+  price: {
+    textAlign: 'right',
+  },
+  positiveText: {
+    color: 'green',
+  },
+  negativeText: {
+    color: 'red',
+  },
+});
+
+export default ({ transactions }) => {
+  const styleClasses = useStyles();
+
+  const renderTransactions = (transactions) => {
+    return transactions.map(t => {
+      // decompose timestamp
+      const ts = new Date(t.timestamp);
+      const date = `${ts.getMonth() + 1}/${ts.getDate()}/${ts.getFullYear()}`;
+      const hour = ts.getHours() === 0 ? 12 : ts.getHours() % 12;
+      const minute = ts.getMinutes();
+      const amPm = ts.getHours() < 12 ? 'AM' : 'PM';
+
+      const plusMinus = t.type === 'buy' ? '-' : '+';
+      const priceColor = t.type === 'buy' 
+        ? styleClasses.negativeText
+        : styleClasses.positiveText;
+
+      return (
+        <Grid item key={t.timestamp}>
+          <Card>
+            <CardContent className={styleClasses.card}>
+              <div className={styleClasses.timestamp}>
+                {`${date} ${hour}:${minute}${amPm}`}
+              </div>
+              <div className={styleClasses.column}>
+                {t.type.toUpperCase()}
+              </div>
+              <div className={styleClasses.column}>
+                {t.symbol}
+              </div>
+              <div className={styleClasses.column}>
+                {`Shares: ${t.shares}`}
+              </div>
+              <div
+                className={
+                  `${styleClasses.column} ${styleClasses.price} ${priceColor}`
+                }
+              >
+                {`${plusMinus}$${t.total.toFixed(2)}`}
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      );
+    });
+  };
+
+  return (
+    <Grid container direction='column' spacing={2}>
+      {renderTransactions(transactions)}
+    </Grid>
+  );
+};
--- client/pages/transactions.js
+++ client/pages/transactions.js
@@ -1,21 +1,50 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import Router from 'next/router';
+import axios from 'axios';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
 
 import UserContext from '../context/UserContext';
 import Navbar from '../components/Navbar';
+import TransactionList from '../components/TransactionList';
+
+const useStyles = makeStyles({
+  heading: {
+    paddingBottom: '20px',
+  },
+});
 
 export default () => {
   const { user } = useContext(UserContext);
+  const [ transactions, setTransactions ] = useState([]);
+
+  const styleClasses = useStyles();
 
   useEffect(() => {
     // redirect if user not logged in
     if (user === '') { Router.replace('/') }
   });
 
+  useEffect(() => {
+    // retrieve transaction history from server
+    if (user) {
+      const transactionsEndpoint = 'api/stocks/transactions';
+      axios.get(transactionsEndpoint)
+        .then(res => { setTransactions(res.data.transactions); })
+        .catch(err => { setTransactions([]); });
+    }
+  }, [ user ]);
+
   return (
     <div>
       <Navbar />
-      Transactions
+      <Container maxWidth='md'>
+        <Typography variant='h4' align='center' className={styleClasses.heading}>
+          Transactions
+        </Typography>
+        <TransactionList transactions={transactions} />
+      </Container>
     </div>
   );
 };
